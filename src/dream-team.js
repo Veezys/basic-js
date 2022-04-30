88ea326ab5eee8d618a99629cb56ba75a20fbe8a
Babu Readymade <breadyma@va-f1.kzn.21-school.ru>
2022-04-30 23:12:30 +0300

    feat: third

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,3 +1,4 @@
+const common = require('mocha/lib/interfaces/common');
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
@@ -13,9 +14,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let shifr = [];
+  let f = 1;
+ 
+    if (members) {
+      f = 1;
+    } else {
+      return false;
+    }
+
+    for (let i = 0; i < members.length; i++) {
+        if (typeof(members[i]) === 'string') {
+         f = 0;
+          for (let j = 0; j < members[i].length; j++) {
+            if (members[i][j] != ' ') {
+                shifr.push(members[i][j].toUpperCase());
+                break ;
+            }
+          }
+        }
+    }
+     shifr.sort();
+    if (f == 0) {
+        return shifr.join('');;
+    } else {
+        return false;
+    }
+    shifr.sort();
+    console.log(shifr);
 }
 
 module.exports = {
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+
+  let s = 0;
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] == '@') {
+        s = i;
+    }
+  }
+  s += 1;
+  return email.slice(s);
 }
 
 module.exports = {
