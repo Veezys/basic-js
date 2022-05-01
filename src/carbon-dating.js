d60e52b8ee0a2e7eeedcdcc40aa0a58e8c4844d6
Babu Readymade <breadyma@pr-n6.kzn.21-school.ru>
2022-05-01 22:36:03 +0300

    feat: 4 update

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string') return false
+  const number = Number(sampleActivity)
+  if (number < 0 || number !== number || number > 15 || number === 0) return false
+  const ln = 0.693
+  const k = ln / HALF_LIFE_PERIOD
+  const res = Math.log(MODERN_ACTIVITY / number) / k
+  return Math.ceil(res)
 }
 
 module.exports = {
