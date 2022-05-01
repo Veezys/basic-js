f3d83a4cae4baa9eed174faa19c063fbe909df2b
Babu Readymade <breadyma@pr-n6.kzn.21-school.ru>
2022-05-01 18:10:17 +0300

    second

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n ) {
+  let text = ['0','1','2', '3', '4', '5', '6', '7', '8', '9','A', 'B', 'C', 'D', 'E', 'F'];
+    
+  let str = n.split('-');
+  str = str.join('')
+  
+  for (let i = 0; i < str.length; i++) {
+      if (text.indexOf(str[i]) == -1) {
+          return false;
+      }
+  }
+  return true;
 }
 module.exports = {
   isMAC48Address
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSumOfDigits(n) {
+  if (n < 10) 
+  return n;
+  if (n == 10) {
+      return 1;
+  }
+  if (n > 10) {
+      return getSumOfDigits(Math.floor(getSumOfDigits(n % 10) + getSumOfDigits(n / 10)));
+  }
 }
 
 module.exports = {
